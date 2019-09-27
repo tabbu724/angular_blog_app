19db3df875f7485d3fc469de3149f09a22146074
tabbu724 <tabbusehgal724@gmail.com>
2019-09-27 10:50:15 +0530

    blog app using anguar 8 and ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import {RouterModule,Routes} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {path:"home",component:HomeComponent},
+      {path:"create",component:BlogCreateComponent},
+      {path:"view",component:BlogViewComponent},
+      {path:"edit",component:BlogEditComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
---
+++ src/app/blog-view/blog-view.component.ts
@@ -0,0 +1,20 @@
+// importing header files
+import { Component, OnInit } from '@angular/core';
+
+
+// decorator
+@Component({
+  selector: 'app-blog-view',
+  templateUrl: './blog-view.component.html',
+  styleUrls: ['./blog-view.component.css']
+})
+
+// class where logic is written
+export class BlogViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
