797d550e73a618c52ef89252bc75559edc5f33f1
tabbu724 <tabbusehgal724@gmail.com>
2019-09-28 17:46:15 +0530

    navbar works now with bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { BlogViewComponent } from './blog-view/blog-view.component';
 import { BlogCreateComponent } from './blog-create/blog-create.component';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
+
+// custom imports
 import {RouterModule,Routes} from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
